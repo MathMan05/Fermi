191ca7c548c2cedcabf415c7e137bd475cc549f5
lizzie <lizzie@eden-emu.dev>
2026-08-01 16:03:27 +0000

    feat: bound guild descs so they dont overflow
    Signed-off-by: lizzie <lizzie@eden-emu.dev>

--- src/webpage/emoji.ts
+++ src/webpage/emoji.ts
@@ -157,9 +157,11 @@ class Emoji {
 					guildText.classList.add("flexttb", "guildEmojiText");
 
 					const guildName = document.createElement("span");
+					guildName.id = "guildName";
 					guildName.textContent = lookup.guild.name;
 
 					const guildDesc = document.createElement("span");
+					guildDesc.id = "guildDesc";
 					const discoverable = lookup.guild.features.find((_) => _ === "DISCOVERABLE");
 					if (discoverable) {
 						if (lookup.guild.description) {
