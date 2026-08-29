5372e936787cc463f3f54e803e313904a63a6a63
Mathium05 <mathium@fermi.chat>
2026-08-29 11:56:49 -0500

    move this line

--- src/webpage/localuser.ts
+++ src/webpage/localuser.ts
@@ -340,7 +340,6 @@ class Localuser {
 		);
 	}
 	async queryBlog() {
-		this.perminfo.localuser ??= {};
 		const prefs = getPreferences();
 		const bstate = prefs.showBlogUpdates;
 		if (bstate === undefined) {
@@ -2120,6 +2119,7 @@ class Localuser {
 		};
 		iconDiv.onclick = () => toggle();
 		this.perminfo.folderStates ??= {};
+		this.perminfo.localuser ??= {};
 		this.perminfo.folderStates[folder.id] ??= {};
 		if (this.perminfo.folderStates[folder.id].state === true) {
 			toggle(true);
